715288d7bde03873367c5b464c80d39169078a39
zahraa-01 <zahraa.karim@ada.ac.uk>
2025-01-13 12:21:06 +0000

    completed 12-hour clock challenge

--- 06. 12-hour-clock/solution.js
+++ 06. 12-hour-clock/solution.js
@@ -1,9 +1,22 @@
 function convertTo12HourClock(time) {
+
   const hours = time.slice(0, 2);
   const minutes = time.slice(-2);
-  if (hours > 12) {
+
+  if (hours === "00") {
+    return `12:${minutes} AM`;
+  }
+  else if (hours.charAt(0) === "0") {
+    let time = hours.substring(1);
+    return `${time}:${minutes} AM`;
+  }
+  else if (hours === "12") {
+    return `12:${minutes} PM`;
+  }
+  else if (hours > 12) {
     return `${hours - 12}:${minutes} PM`;
-  } else {
+  }
+  else {
     return `${hours}:${minutes} AM`;
   }
 }
