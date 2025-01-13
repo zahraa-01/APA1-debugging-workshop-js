33b943caf198020b7f38e50034d27e1e4a565a8f
zahraa-01 <zahraa.karim@ada.ac.uk>
2025-01-13 11:06:10 +0000

    completed Capitalise challenge

--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,6 +1,10 @@
 function capitalise(word) {
-  word[0] = word[0].toUpperCase();
-  return word;
+  if (/[A-Z]/.test(word) === true) {
+    let lowerCase = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + lowerCase.slice(1);
+  } else {
+    return word[0].toUpperCase() + word.slice(1);
+  }
 }
 
 module.exports = { capitalise };
