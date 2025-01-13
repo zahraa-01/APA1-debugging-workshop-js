41bd925d2dee359b8e2a8277c038a7556cde0f85
zahraa-01 <zahraa.karim@ada.ac.uk>
2025-01-13 13:14:37 +0000

    completed fizzbuzz challenge

--- 11. fizz-buzz/solution.js
+++ 11. fizz-buzz/solution.js
@@ -1,13 +1,13 @@
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
       console.log("Fizz");
     } else if (i % 5 === 0) {
       console.log("Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
     } else {
-      console.log(i);
+      console.log(i.toString());
     }
   }
 }
