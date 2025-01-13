05c4a1d52d212e1dc127ca7f6d55b204a764082c
zahraa-01 <zahraa.karim@ada.ac.uk>
2025-01-13 13:06:10 +0000

    completed rotate angle by degrees challenge

--- 09. rotate-angle/solution.js
+++ 09. rotate-angle/solution.js
@@ -1,8 +1,7 @@
 function rotateAngleByDegrees(initialAngle, rotation) {
   // Calculate the new angle
-  let newAngle = initialAngle + (rotation % 360);
-
-  return newAngle;
+  let newAngle = (initialAngle + rotation) % 360;
+  return newAngle < 0 ? newAngle + 360 : newAngle;
 }
 
 module.exports = { rotateAngleByDegrees };
